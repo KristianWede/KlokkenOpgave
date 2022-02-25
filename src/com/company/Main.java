7fb4a93755ca19cc6cfc89f6cf84a5651c2fbf46
KristianWede <kris79s4@stud.kea.dk>
2022-02-25 15:05:59 +0100

    Did some cleaning up for delivery

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,12 +7,13 @@ public class Main {
     String[] alleMåneder = {"Januar", "Februar", "Marts", "April", "Maj", "Juni", "Juli", "August", "September", "Oktober", "November", "December"};
     String[] alleDage = {"Mandag","Tirsdag","Onsdag", "Torsdag", "Fredag", "Lørdag", "Søndag"};
 
-    public void dagsDato(int weekDay, int day, int month, int year){
+    public String dagsDato(int weekDay, int day, int month, int year){
 
         String ugeDag = alleDage[weekDay-1];
         String månedsNavn = alleMåneder[month-1];
+        String nutiden = "\t I dag er det " + ugeDag + " d." + day + " " + månedsNavn + " " + year;
 
-        System.out.printf("\t " + "I dag er det %s d. %d %s %d.", ugeDag, day, månedsNavn, year);
+        return nutiden;
     }
 
     public static void main(String[] args) {
@@ -23,41 +24,12 @@ public class Main {
 
         int numberForDayName = today.getDayOfWeek().getValue();
 
-        System.out.println(numberForDayName);
-
         int numberForDay = today.getDayOfMonth();
-
-        System.out.println(numberForDay);
 
         int numberForMonth = today.getMonthValue();
 
-        System.out.println(numberForMonth);
-
         int numberForYear = today.getYear();
 
-        System.out.println(numberForYear);
-
-        obj.dagsDato(numberForDayName, numberForDay, numberForMonth, numberForYear);
-
-
-
-
-/*
-        if( dage >= 7 ) {
-            int konvertér = dage % 7;
-            System.out.println(år);
-            System.out.println(alleMåneder[måned-1]);
-            System.out.println(alleDage[konvertér]);
-        } else {
-            System.out.println(år);
-            System.out.println(alleMåneder[måned - 1]);
-            System.out.println(alleDage[dage]);
-        }
-*/
-
-
-     //System.out.println("I dag er det måned: " + tekstVerMåned.substring());
-
-        //Hvordan kan en String blive til en Int? Med Integer.parseInt
+        System.out.println(obj.dagsDato(numberForDayName, numberForDay, numberForMonth, numberForYear));
     }
 }
