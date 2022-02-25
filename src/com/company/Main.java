b24c1d2d732448741fd2f3bd024aa65e620436e9
KristianWede <kris79s4@stud.kea.dk>
2022-02-25 14:00:02 +0100

    Start af Program

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,46 @@ import java.time.LocalDate;
 
 public class Main {
 
+    public int dagsDato(int weekDay, int date, int month, int year){
+        return 0;
+    }
+
+    public int getDayOfWeek(LocalDate today){
+
+        String tekstForDate = today.toString();
+
+        int part1OfDay = tekstForDate.lastIndexOf("-");
+
+        int part2OfDay = tekstForDate.length();
+
+        String tekstForToday = tekstForDate.substring(part1OfDay+1,part2OfDay);
+
+        int value = Integer.parseInt(tekstForToday);
+
+        return value;
+    }
+
+    public void getMonth (){
+
+    }
+
+    public void getDayOfMonth(){
+
+    }
+
+    public void getYear(){
+
+    }
+
+    public int getValue(int value){
+
+        return value;
+    }
+
     public static void main(String[] args) {
+
+        Main obj = new Main();
+
 
         String[] alleMåneder = {"Januar", "Februar", "Marts", "April", "Maj", "Juni", "Juli", "August", "September", "Oktober", "November", "December"};
         String[] alleDage = {"Mandag","Tirsdag","Onsdag", "Torsdag", "Fredag", "Lørdag", "Søndag"};
@@ -13,12 +52,16 @@ public class Main {
 
         String tekstToday = today.toString();
 
-        System.out.println(tekstToday.indexOf("-"));
+        System.out.println(today);
 
+        System.out.println();obj.getDayOfWeek(today);
+
+        System.out.println(tekstToday.indexOf("-"));
+        System.out.println(tekstToday.lastIndexOf("-"));
 
         int år = Integer.parseInt(tekstToday.substring(0,4));
         int måned = Integer.parseInt(tekstToday.substring(5,7));
-        int dage = Integer.parseInt(tekstToday.substring(8,10));
+
 
         if( dage >= 7 ) {
             int konvertér = dage % 7;
